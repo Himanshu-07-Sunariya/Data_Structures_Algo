07da79b6747700105d865027a0adde6e2c988da3
Himanshu Sunariya <himanshu.sunariya24@gmail.com>
2025-12-30 12:28:17 +0530

    Add files via upload

--- DP/house_robber1.cpp
+++ DP/house_robber1.cpp
@@ -1,3 +1,5 @@
+https://leetcode.com/problems/house-robber/
+
 // Approach 1:- recursive approach(include -exclude)
 
 int robbery(vector<int> &nums,int index){
@@ -132,4 +134,115 @@ int solveUsingOptimal(vector <int> &nums,int index,int n){
 
         int ans=solveUsingOptimal(nums,index,n);
         return ans;
+    }
+
+
+
+// REVISION
+
+int helper(int ind,vector <int> &nums){
+        // base case
+        if(ind>=nums.size()) return 0;
+
+        // pick/not pick
+        // glt index access n ho isliye ...if condition so use it in BU approach
+        int include=nums[ind]+helper(ind+2,nums);
+
+        int exclude=helper(ind+1,nums);
+
+        return max(include,exclude);
+    }
+
+    int helper_TD(int ind,vector <int> &nums,vector <int> &dp){
+        // base case
+        if(ind>=nums.size()) return 0;
+
+        // dp step2
+        if(dp[ind]!=-1) return dp[ind];
+
+        // pick/not pick
+        // glt index access n ho isliye ...if condition so use it in BU approach
+        int include=nums[ind]+helper_TD(ind+2,nums,dp);
+
+        int exclude=helper_TD(ind+1,nums,dp);
+
+        dp[ind]=max(include,exclude);
+        return dp[ind];
+    }
+
+    int helper_BU(vector <int> &nums){
+        // 1.Create dp array
+        vector <int> dp(nums.size()+1,-1);
+
+        // 2. base case fill
+        dp[nums.size()]=0;
+
+        // 3. Cell meaning
+        // consider array from ith index to n (considering only those houses) and stores max profit that can be decided if we want to include or exclude that house to do chori...
+
+        // 4.iterative work
+        for(int i=nums.size()-1; i>=0; i--){
+            // pick/not pick
+            // glt index access n ho isliye ...if condition so use it in BU approach
+            int include=nums[i];
+            // nice step
+
+            if(i+2<nums.size()){
+                include=nums[i]+dp[i+2];
+            }
+
+            int exclude=dp[i+1];
+
+            dp[i]=max(include,exclude);
+        }
+
+        return dp[0];
+    }
+
+    int helper_BUSO(vector <int> &nums){
+        // 1.Create dp array
+        // vector <int> dp(nums.size()+1,-1);
+
+        int next1=0;
+        int next2=0;
+        int curr;
+
+        // 2. base case fill
+        // dp[nums.size()]=0;
+
+        // 3. Cell meaning
+        // consider array from ith index to n (considering only those houses) and stores max profit that can be decided if we want to include or exclude that house to do chori...
+
+        // 4.iterative work
+        for(int i=nums.size()-1; i>=0; i--){
+            // pick/not pick
+            // glt index access n ho isliye ...if condition so use it in BU approach
+            int include=nums[i];
+            // nice step
+
+            if(i+2<nums.size()){
+                include=nums[i]+next2;
+            }
+
+            int exclude=next1;
+
+            curr=max(include,exclude);
+
+            // shifting dhng se kr no extra change wali bc
+            next2=next1;
+            next1=curr;
+        }
+
+        return curr;
+    }
+
+    int rob(vector<int>& nums) {
+        // int ind=0;
+        // return helper(ind,nums);
+        // vector <int> dp(nums.size()+1,-1);
+        // return helper_TD(ind,nums,dp);
+
+        // there is no use of index variable here in Bottom Up
+
+        return helper_BUSO(nums);
     }
